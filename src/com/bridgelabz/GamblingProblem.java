ff1a1203e35e35569aa1f4dd294c91b393b5c873
10011999 <motewaraditya77@gmail.com>
2022-08-01 17:55:09 +0530

    Update:code of Variables in Gambling

---
+++ src/com/bridgelabz/GamblingProblem.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class GamblingProblem {
+
+    public static void main(String[] args) {
+        int perDayStake = 100;
+        final int StakePerGame = 1;
+        
+    }
+    }
+
